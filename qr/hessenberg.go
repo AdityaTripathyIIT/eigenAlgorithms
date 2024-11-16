75f4812c5f10ddb1325f2b8f61b44cc5afc66c33
= <=>
2024-11-16 15:41:33 +0530

    1st iteration

--- qr/hessenberg.go
+++ qr/hessenberg.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"math/cmplx"
 )
 
-// Compute the squared norm of a complex vector (sum of squared absolute values)
-func norm(v []complex128) float64 {
-	var sum float64
-	for _, val := range v {
-		sum += cmplx.Abs(val) * cmplx.Abs(val)
-	}
-	return sum
-}
-
-// Hessenberg transformation using Householder reflections
 func hessenberg(A [][]complex128) [][]complex128 {
 	n := len(A)
 	H := make([][]complex128, n)
@@ -36,12 +25,11 @@ func hessenberg(A [][]complex128) [][]complex128 {
 		}
 
 		if cmplx.Abs(u[0]) < 1e-15 {
-			u[0] += normU
+			u[0] += 0
 		} else {
-			u[0] += cmplx.Conj(u[0]) / complex(cmplx.Abs(u[0]), 0) * normU
+			u[0] += (cmplx.Conj(u[0]) / complex(cmplx.Abs(u[0]), 0)) * normU
 		}
 
-		//beta := complex(cmplx.Abs(u[0]), 0)
         normalizingFactor := cmplx.Sqrt(complex(norm(u), 0))
 		for i := 0; i < len(u); i++ {
 			u[i] /= normalizingFactor
@@ -49,55 +37,27 @@ func hessenberg(A [][]complex128) [][]complex128 {
 
 		for j := k; j < n; j++ {
 			sum := complex(0, 0)
-			for i := range u {
-				sum += cmplx.Conj(u[i]) * H[k+1+i][j]
+			for i := 0; i < len(u); i++ {
+				sum += cmplx.Conj(u[i]) * H[k+i+1][j]
 			}
 
-			for i := range u {
-				H[k+1+i][j] -= 2 * u[i] * sum
+			for i := 0; i < len(u); i++ {
+				H[k+i+1][j] -= 2 * u[i] * sum
 			}
 		}
 
 		for i := 0; i < n; i++ {
-			// Compute the scalar product (H[i][k+1:n] * u)
 			sum := complex(0, 0)
-			for j := range u {
+			for j := 0; j < len(u); j++ {
 				sum += H[i][k+1+j] * u[j]
 			}
 
-			for j := range u {
-				H[i][k+1+j] -= 2 * cmplx.Conj(u[j]) * sum
+			for j := 0; j < len(u); j++ {
+				H[i][k+j+1] -= 2 * cmplx.Conj(u[j]) * sum
 			}
 		}
 	}
 
 	return H
-}
-
-// Print a complex matrix in a readable format
-func printMatrix(matrix [][]complex128) {
-	for _, row := range matrix {
-		for _, value := range row {
-			fmt.Printf("%6.2f +%6.2fi ", real(value), imag(value))
-		}
-		fmt.Println()
-	}
-}
-
-func main() {
-	matrix := [][]complex128{
-		{complex(4, 0), complex(1, 0), complex(3, 0), complex(4, 0)},
-		{complex(1, 0), complex(2, 0), complex(4, 0), complex(0, 0)},
-		{complex(3, 0), complex(4, 0), complex(5, 0), complex(1, 0)},
-		{complex(4, 0), complex(0, 0), complex(1, 0), complex(4, 0)},
-	}
-
-	fmt.Println("Original Matrix:")
-	printMatrix(matrix)
-
-	hessenbergMatrix := hessenberg(matrix)
-
-	fmt.Println("Upper Hessenberg form:")
-	printMatrix(hessenbergMatrix)
 }
 
--- script.py
+++ script.py
@@ -1,17 +1,45 @@
 import numpy as np
 from scipy.linalg import hessenberg
-
+from scipy.linalg import qr
+from pprint import pprint
 # Define the complex matrix A
-A = np.array([[4+0j, 1+0j, 3+0j, 4+0j],
-              [1+0j, 2+0j, 4+0j, 0+0j],
-              [3+0j, 4+0j, 5+0j, 1+0j],
-              [4+0j, 0+0j, 1+0j, 4+0j]])
+'''
+A = np.array([[5+1j, 9+6j, 3+2j, 5+1j, 1+1j],
+              [3+2j, 3+7j, 0+4j, 2+3j, 2+4j],
+              [6+3j, 2+8j, 8+6j, 9+5j, 3+7j],
+              [8+4j, 7+9j, 1+8j, 8+7j, 4+2j],
+              [5+5j, 2+0j, 6+0j, 7+9j, 5+5j]])
+A = np.array([[5+1j, 9+6j, 3+9j, 5+8j],
+              [3+2j, 3+7j, 0+7j, 2+6j],
+              [6+3j, 2+8j, 8+5j, 9+4j],
+              [8+4j, 7+9j, 1+3j, 8+2j]])
 
+A = np.array([[5+0j, 9+0j, 3+0j, 5+0j],
+              [3+0j, 3+0j, 0+0j, 2+0j],
+              [6+0j, 2+0j, 8+0j, 9+0j],
+              [8+0j, 7+0j, 1+0j, 8+0j]])
+'''
+A = np.array([
+    [complex(2, 0), complex(3, 0), complex(5, 0), complex(7, 0), complex(11, 0)],
+    [complex(13, 0), complex(17, 0), complex(19, 0), complex(23, 0), complex(29, 0)],
+    [complex(31, 0), complex(37, 0), complex(41, 0), complex(43, 0), complex(47, 0)],
+    [complex(53, 0), complex(59, 0), complex(61, 0), complex(67, 0), complex(71, 0)],
+    [complex(73, 0), complex(79, 0), complex(83, 0), complex(89, 0), complex(97, 0)]
+])
 # Compute the Hessenberg form using SciPy's 'hessenberg' function
 H = hessenberg(A)
-eigenvalues_np = np.linalg.eigvals(H)
-print("Eigenvalues using NumPy:", eigenvalues_np)
-eigenvalues_np_1 = np.linalg.eigvals(A)
-print("Eigenvalues using NumPy:", eigenvalues_np_1)
+#print("Upper hessenberg:")
+#pprint(H)
+eig1, _= np.linalg.eig(H)
+#print("Eigen values of upper hessenberg matrix")
+pprint(eig1)
+Q, R = qr(H)
+#print("Upper triangular:")
+pprint(R)
+#eig2, _ = np.linalg.eig(A)
+#print("Eigen values of original matrix")
+pprint(eig2)
+print("\n")
 
-print(H)
+
+
